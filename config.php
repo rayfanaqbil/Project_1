e1f810b42119ad748ecf8cf9d5d15f111e013eea
rayfanaqbil <rayfana09@gmail.com>
2023-05-25 14:33:16 +0700

    proyek 1

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$server     = "localhost";
+$user       = "root";
+$password   = "";
+$db         = "canteen";
+
+$koneksi    = mysqli_connect($server, $user, $password, $db);
+if (!$koneksi) {
+    die("Tidak bisa terkoneksi ke database" . mysqli_connect_error());
+}
